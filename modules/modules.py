52784958eaa7480db69bab43841981b9e7dc641b
Ali Masarwe <33619630+AliMasarweh@users.noreply.github.com>
2020-02-12 14:17:31 +0200

    updated modules to return cheapest basket as a basket object and returns also the missing items

--- modules/modules.py
+++ modules/modules.py
@@ -44,11 +44,21 @@ def add_items_to_store_basket_new_setup(items_names, store, store_basket, store_
             store_basket[store_name].add_item_new_setup(store, item)
 
 
+def find_missing_items(items_names, store_basket, cheapest_store_name):
+    ans = []
+    for item in items_names:
+        if item not in store_basket[cheapest_store_name].item_to_price:
+            ans.append(item)
+
+    return ans
+
+
 def cheapest_basket(items_names):
     stores = queries.getDictionaryofStores()
     store_basket = {}
     max_items = 0
     cheapest_price = sys.maxsize
+    cheapest_store_name = None
 
     for store_name in stores:
         store = stores[store_name]
@@ -63,10 +73,15 @@ def cheapest_basket(items_names):
         cheapest_price = min(cheapest_price, store_basket[store_name].price())
         if store_basket[store_name].price() > cheapest_price:
             del store_basket[store_name]
+        else:
+            cheapest_store_name = store_name
 
-    return store_basket
+    missing_items = find_missing_items(items_names, store_basket, cheapest_store_name)
+
+    return store_basket[cheapest_store_name], missing_items
 
 
 if __name__ == '__main__':
-    basket = cheapest_basket(['Apple Juice', 'Apple Sauce'])
+    basket, missing = cheapest_basket(['Apple Juice', 'Apple Sauce'])
     print(basket)
+    print(missing)
