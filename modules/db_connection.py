058cec2c0a80cb2d3af5040610a810138ce1184a
Ali Masarwe <33619630+AliMasarweh@users.noreply.github.com>
2020-02-13 00:21:23 +0200

    database for notification is done

--- modules/db_connection.py
+++ modules/db_connection.py
@@ -12,9 +12,34 @@ connection = pymysql.connect(
 )
 
 
-def setDiscountAndMinQuantity(store_name, item_to_discount_with_min_quantity):
-    store = queries.getStoresProductsList(store_name)
+def setDiscountAndMinQuantity(store_name, item_to_quant_disc):
+    store = queries.getDictionaryofStoresWithQuantityDiscount(store_name)[store_name]
+    try:
+        with connection.cursor() as cursor:
+            for item in store:
+                if item in item_to_quant_disc:
+                    discount = item_to_quant_disc[item]['discount']
+                    min_quant = item_to_quant_disc[item]['min_quantity']
+
+                    query = f'update Stores_Products SET Discount = {discount}, MinQuantity = {min_quant} where ' \
+                            f'ProductID = (select ProductID from Products where ProductName = "{item}")' \
+                            f'and StoreID = (select StoreID from Stores where StoreName = "{store_name}")'
+                    cursor.execute(query)
+                connection.commit()
+    except Exception as e:
+        print(e)
+        print("Could not insert stores into DB")
+
+def getUsersAndTypes():
+    try:
+        with connection.cursor() as cursor:
+            query = 'select * from Users'
+            cursor.execute(query)
+            return cursor.fetchall()
+    except Exception as e:
+        print(e)
+        print("Could not insert stores into DB")
 
 
 if __name__ == '__main__':
-    print(queries.getDictionaryofStoresWithQuantityDiscount('Walmart'))
+    setDiscountAndMinQuantity('Walmart', {'Aluminum  Foil': {'discount': 10.0, 'min_quantity': 2}})
--- modules/notifier.py
+++ modules/notifier.py
@@ -1,11 +1,63 @@
+import pymysql
+import json
 from mohammad.hackathon_queries import queries
 
-def announce_sale(chat_id: int, item_to_discount_with_min_quantity):
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="123123",
+    db="hackathon",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def setDiscountAndMinQuantity(store_name, item_to_quant_disc):
+    store = queries.getDictionaryofStoresWithQuantityDiscount(store_name)[store_name]
+    try:
+        with connection.cursor() as cursor:
+            for item in store:
+                if item in item_to_quant_disc:
+                    discount = item_to_quant_disc[item]['discount']
+                    min_quant = item_to_quant_disc[item]['min_quantity']
+
+                    query = f'update Stores_Products SET Discount = {discount}, MinQuantity = {min_quant} where ' \
+                            f'ProductID = (select ProductID from Products where ProductName = "{item}")' \
+                            f'and StoreID = (select StoreID from Stores where StoreName = "{store_name}")'
+                    cursor.execute(query)
+                connection.commit()
+    except Exception as e:
+        print(e)
+        print("Could not insert stores into DB")
+
+def getUsersAndTypes():
+    try:
+        with connection.cursor() as cursor:
+            query = 'select * from Users'
+            cursor.execute(query)
+            return cursor.fetchall()
+    except Exception as e:
+        print(e)
+        print("Could not insert stores into DB")
+
+def announce_sale(chat_id: int, item_to_quant_disc):
     store_name = queries.getUserTypeByChatId(chat_id)
     if store_name == "client":
-        return "A mere client shouldn't try this thing!"
+        return "A mere client shouldn't access discount!"
+    setDiscountAndMinQuantity(store_name, item_to_quant_disc)
+    notify_sales(item_to_quant_disc)
 
 
+def notify_sales(item_to_quant_disc):
+    users = getUsersAndTypes()
+    for user in users:
+        print(user)
+        print(user['ChatId'], user['UserType'])
+        if user['UserType'] == 'client':
+            pass
+            # notify_user(user['ChatId'], item_to_quant_disc)
 
-def notify_sales(chat_id: int):
-    pass
+
+if __name__ == '__main__':
+    setDiscountAndMinQuantity('Walmart', {'Aluminum  Foil': {'discount': 10.0, 'min_quantity': 2}})
+    notify_sales(None)
